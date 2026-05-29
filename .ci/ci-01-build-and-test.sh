59ee8e352552c1fed1e48456bc2d612c4b4a3279
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-29 23:52:00 +0200

    Compilation works

--- .ci/ci-01-build-and-test.sh
+++ .ci/ci-01-build-and-test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 REPO_ROOT=$(realpath $(dirname ${BASH_SOURCE[0]})/..)
-TMPDIR="$(mktemp -d)"
-trap 'rm -rf "${TMPDIR}"' EXIT
-cmake -B $TMPDIR -S $REPO_ROOT
-cmake --build $TMPDIR
+# TMPDIR="$(mktemp -d)"
+# trap 'rm -rf "${TMPDIR}"' EXIT
+TMPDIR=build
+cmake \
+    -DCMAKE_C_FLAGS=-fcommon `# gcc-14 seem to need fcommon for legacy COMMON block behaviour ` \
+    -DCMAKE_GENERATOR=Ninja \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -S $REPO_ROOT \
+    -B $TMPDIR \
+    --fresh
+    
+cmake --build $TMPDIR --parallel
+ctest --test-dir $TMPDIR --output-on-failure
