7330df7b2258c9c0db90878c1e562dba77e0e6f4
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-30 00:01:37 +0200

    Make tests pass

--- .ci/ci-01-build-and-test.sh
+++ .ci/ci-01-build-and-test.sh
@@ -12,4 +12,9 @@ cmake \
     --fresh
     
 cmake --build $TMPDIR --parallel
+
+# among the tests: xeigtstz puts very large doublecomplex arrays on the stack;
+# with the default 8 MB stack it segfaults before writing output, hence we make
+# the stack size unlimited:
+ulimit -s unlimited
 ctest --test-dir $TMPDIR --output-on-failure
---
+++ AGENTS.md
@@ -0,0 +1,2 @@
+# clapack
+This is a f2c converted Lapack version 3.2.1 C source project. It has modernized cmake build system and a number of patches.
