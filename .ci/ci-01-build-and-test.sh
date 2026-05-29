726782eab40ed927423dd299416985d5738944e4
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-29 23:36:57 +0200

    Typo

--- .ci/ci-01-build-and-test.sh
+++ .ci/ci-01-build-and-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO_ROOT=$(realpath $(dirname ${BASH_SOURCE[0]}))
+REPO_ROOT=$(realpath $(dirname ${BASH_SOURCE[0]})/..)
 TMPDIR="$(mktemp -d)"
 trap 'rm -rf "${TMPDIR}"' EXIT
 cmake -B $TMPDIR -S $REPO_ROOT
