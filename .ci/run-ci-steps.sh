26eebb28f13077eda0504aa5cc675eef15b3e9cd
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-31 11:35:18 +0200

    Memory sanitizer

--- .ci/run-ci-steps.sh
+++ .ci/run-ci-steps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 .ci/ci-01-build-and-test.sh
 CC=clang CFLAGS=-fsanitize=address VARIANT=asan .ci/ci-01-build-and-test.sh
+CC=clang CFLAGS=-fsanitize=memory VARIANT=msan .ci/ci-01-build-and-test.sh
